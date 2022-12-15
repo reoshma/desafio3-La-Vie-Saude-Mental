980b32ff099353787c913db16f0e7c70d0223b07
Renan Oshima <renanoshima@gmail.com>
2022-12-15 17:06:06 -0300

    correção

--- src/validations/usuarios/create.js
+++ src/validations/usuarios/create.js
@@ -4,7 +4,7 @@ module.exports = validate({
   body: Joi.object({
     nome: Joi.string().required(),
     email: Joi.string().email().required(),
-    senha: Joi.string().min(8).required(),
+    senha: Joi.string().min(6).required(),
     apresentacao: Joi.string(),
   }),
 });
